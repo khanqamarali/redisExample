59d48ab785cbdac8410e5a56cce103770ca9d6d1
qamarali <qamarali@qamaralis-MacBook-Pro.local>
2017-08-06 19:22:08 -0500

    POC of redis

---
+++ redisPOC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ServiceStack;
+using ServiceStack.Text;
+using ServiceStack.Redis;
+using ServiceStack.DataAnnotations;
+using StackExchange.Redis;
+
+namespace redisPOC
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            using(var cm = ConnectionMultiplexer.Connect(""))
+            {
+				var db = cm.GetDatabase();
+				//db.StringSet("key", "QamarAli");
+				var key = db.StringGet("key");
+                Console.WriteLine(key);
+
+            }
+			
+          //  Console.WriteLine("Hello World!");
+        }
+    }
+}
